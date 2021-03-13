f9e76851e9370e099e35e54d6e7c882548040654
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-13 17:54:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from urllib.request import urlretrieve
 urll = ("http://dl.dropboxusercontent.com/s/ecl4tj6q2u8s4q3/fig-03_5.png?raw=1")
 filenamee = "fig-03_5.png"
 urlretrieve(urll,filenamee)
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown(filenamee)
 
 st.markdown(backgroundimage, unsafe_allow_html=True)
 url = 'http://dl.dropboxusercontent.com/s/kcmjcwxjtxnpt5n/malaria.pkl?raw=1'
